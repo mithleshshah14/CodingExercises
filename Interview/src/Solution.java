119c876179778555dea431cac819bf94033b09fe
mithleshshah84@gmail.com <mithleshshah84@gmail.com>
2023-07-31 01:50:28 +0530

    Added few coding problems

---
+++ Interview/src/Solution.java
@@ -0,0 +1,59 @@
+
+/**
+ * Definition for singly-linked list.
+ */
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+class Solution {
+
+    public void reorderList(ListNode head) {
+
+        //Find middle of list using a slow and fast pointer approach
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //Reverse the second half of the list using a tmp variable
+        ListNode second = slow.next;
+        ListNode prev = slow.next = null;
+        while (second != null) {
+            ListNode tmp = second.next;
+            second.next = prev;
+            prev = second;
+            second = tmp;
+        }
+
+        //Re-assign the pointers to match the pattern
+        ListNode first = head;
+        second = prev;
+        while (second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+        solution.reorderList(node);
+    }
+}
+
