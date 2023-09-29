b5ded75f16188b0b8550b0b0f80079aaa81c8387
mithleshshah84@gmail.com <mithleshshah84@gmail.com>
2023-09-29 21:19:25 +0530

    Added coding problems for GenerateParanthesisClass

---
+++ Interview/src/GenerateParanthesisClass.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+public class GenerateParanthesisClass {
+
+    public static void main(String[] args) {
+        GenerateParanthesisClass sol = new GenerateParanthesisClass();
+        sol.generateParenthesis(3);
+    }
+
+    Stack<Character> stack = new Stack<>();
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        backtrack(0, 0, n);
+        return res;
+    }
+
+    private void backtrack(int openN, int closedN, int n) {
+        if (openN == closedN && closedN == n) {
+            Iterator vale = stack.iterator();
+            String temp = "";
+            while (vale.hasNext()) {
+                temp = temp + vale.next();
+            }
+            res.add(temp);
+        }
+        if (openN < n) {
+            stack.push('(');
+            backtrack(openN + 1, closedN, n);
+            stack.pop();
+        }
+        if (closedN < openN) {
+            stack.push(')');
+            backtrack(openN, closedN + 1, n);
+            stack.pop();
+        }
+    }
+}
+
